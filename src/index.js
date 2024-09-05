85c8aa3ffc53c41308966deddf5e17b9192921cd
Hoanghaaii <haivu04112003@gmail.com>
2024-09-05 10:11:35 +0700

    Organizing code

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ const port = 3000;
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const route= require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 // Cấu hình express-handlebars
 app.engine('hbs', exphbs.engine({
   extname: 'hbs'
@@ -18,13 +23,11 @@ app.use(morgan('combined'));
 // Đặt thư mục chứa các template
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
+// Kết nối với route
+route(app);
+
 // Định nghĩa route và render template
-app.get('/', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
